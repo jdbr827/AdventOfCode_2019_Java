054887d40a248e462a3b90107ef1857ad23b645e
Jake <reinhartjacob827@gmail.com>
2020-12-01 17:20:44 -0500

    2019 Day 2 Part 2

--- src/year_2019/Day2.java
+++ src/year_2019/Day2.java
@@ -2,47 +2,62 @@ package year_2019;
 
 public class Day2 {
 
+    static int[] real = {1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,6,23,2,13,23,27,1,27,13,31,1,9,31,35,1,35,9,39,1,39,5,43,2,6,43,47,1,47,6,51,2,51,9,55,2,55,13,59,1,59,6,63,1,10,63,67,2,67,9,71,2,6,71,75,1,75,5,79,2,79,10,83,1,5,83,87,2,9,87,91,1,5,91,95,2,13,95,99,1,99,10,103,1,103,2,107,1,107,6,0,99,2,14,0,0};
+
+
     public static void main(String[] args) {
-        int[] test1 = {1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50};
-        /* Part 1 Test */
-        System.out.println(IntCode.createAndRun(test1));
 
-        int[] test2 = {1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,6,23,2,13,23,27,1,27,13,31,1,9,31,35,1,35,9,39,1,39,5,43,2,6,43,47,1,47,6,51,2,51,9,55,2,55,13,59,1,59,6,63,1,10,63,67,2,67,9,71,2,6,71,75,1,75,5,79,2,79,10,83,1,5,83,87,2,9,87,91,1,5,91,95,2,13,95,99,1,99,10,103,1,103,2,107,1,107,6,0,99,2,14,0,0};
-        test2[1] = 12;
-        test2[2] = 2;
-        System.out.println(IntCode.createAndRun(test2));
 
-//        int[] test2 = {
-//                1,0,0,3,
-//                1,1,2,3,
-//                1,3,4,3,
-//                1,5,0,3,
-//                2,10,1,19,
-//                1,19,6,23,
-//                2,13,23,27,
-//                1,27,13,31,
-//                1,9,31,35,
-//                1,35,9,39,
-//                1,39,5,43,
-//                2,6,43,47,
-//                1,47,6,51,
-//                2,51,9,55,
-//                2,55,13,59,
-//                1,59,6,63,
-//                1,10,63,67,
-//                2,67,9,71,
-//                2,6,71,75,
-//                1,75,5,79,
-//                2,79,10,83,
-//                1,5,83,87,
-//                2,9,87,91,
-//                1,5,91,95,
-//                2,13,95,99,
-//                1,99,10,103,
-//                1,103,2,107,
-//                1,107,6,0,
-//                99,
-//                2,14,0,0};
 
+        /* Part 1 */
+        int[] p1 = real.clone();
+        p1[1] = 12;
+        p1[2] = 2;
+        System.out.println(IntCode.createAndRun(p1).getMemory()[0] == 2846248);
+
+
+        /* Part 2 */
+
+        /*
+        The first four instructions only write to address 3, and address 3 is not read from again after 4th instruction,
+        so we can effectively ignore those first four instructions (they don't affect what address 0 becomes)
+
+        All the next instructions before the halt write to the address of their third param, except for the ultimate
+        one which writes to address 0. Furthermore, address 1 is read in the first such instruction, and address 2 is
+        not read until the second-to-last such instruction (and, from previous, neither is written to).
+
+        So, we have:
+        -> 4 instructions of effective no-ops
+        -> (A lot - 2) instructions that effectively output address103 = f(address1) for some very convoluted f
+        -> address107 = address103 + address2
+        -> address0 = address107 + address6
+
+        Considering that address6=2 (recall the "first" address is address0), and substituting, we have
+
+        address0 = f(address1) + address2 + 2, for some known and convoluted function f.
+
+        For this problem, then, we need address1 and address 2 s.t.
+            19690720 = f(address1) + address2 + 2
+        ==> 19690718 = f(address1) + address2
+
+        Now consider that, because all of the instructions are either adds or multiples, and all the numbers are
+        positive integers (proven by a quick scan), f(address1) is strictly increasing.
+
+        Additionally, note that address2 has to be some number between 0 and 116 (the last address of the input) so that
+        we can execute the first instruction (1, 0, 0, 3) without going out of bounds
+
+        Finally, recall that we can compute f(address1) for any value of address1 simply by running the program and
+        looking at address103.
+
+        This suggests the following algorithm:
+        */
+
+        int a1 = 0;
+        int a2;
+        do {
+            a1 += 1;
+            real[1] = a1;
+        } while ((a2 = 19690718 - IntCode.createAndRun(real).getMemory()[103]) >= 116);
+        System.out.println((a1 * 100) + a2 == 9074);
     }
 }
--- src/year_2019/IntCode.java
+++ src/year_2019/IntCode.java
@@ -4,11 +4,35 @@ import java.util.Arrays;
 
 public class IntCode {
 
+    public class Instruction {
+        int opcode;
+        Integer[] params;
+
+        public Instruction (int opcode, Integer... params) {
+            this.opcode = opcode;
+            this.params = params;
+        }
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IntCode intCode = (IntCode) o;
+        return Arrays.equals(getMemory(), intCode.getMemory());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getMemory());
+    }
 
     int[] memory;
+    int instructionPointer = 0;
 
     public IntCode(int[] memory) {
-        this.memory = memory;
+        this.memory = memory.clone();
     }
 
     @Override
@@ -34,26 +58,51 @@ public class IntCode {
      * Runs the Intcode program
      */
     public void run() {
-        int curr_idx = 0;
+        instructionPointer = 0;
         int opcode;
-        while ((opcode = memory[curr_idx]) != 99) {
-            System.out.println(this);
+        while ((opcode = memory[instructionPointer]) != 99) {
             switch (opcode) {
                 case 1:
-                    int addend1 = memory[memory[curr_idx + 1]];
-                    int addend2 = memory[memory[curr_idx + 2]];
-                    memory[memory[curr_idx + 3]] = addend1 + addend2;
-                    curr_idx += 4;
+                    int addend1 = memory[memory[instructionPointer + 1]];
+                    int addend2 = memory[memory[instructionPointer + 2]];
+                    memory[memory[instructionPointer + 3]] = addend1 + addend2;
+                    instructionPointer += 4;
                     break;
                 case 2:
-                    int mult1 = memory[memory[curr_idx + 1]];
-                    int mult2 = memory[memory[curr_idx + 2]];
-                    memory[memory[curr_idx + 3]] = mult1 * mult2;
-                    curr_idx += 4;
+                    int mult1 = memory[memory[instructionPointer + 1]];
+                    int mult2 = memory[memory[instructionPointer + 2]];
+                    memory[memory[instructionPointer + 3]] = mult1 * mult2;
+                    instructionPointer += 4;
                     break;
                 default:
                     throw new Error("Unexpected Opcode");
             }
         }
+    }
+
+    public static boolean test_IntCode_one(int[] input, int[] expected_output) {
+        return IntCode.createAndRun(input).getMemory() == expected_output;
+    }
+
+    public static void test_IntCode_all() {
+        int[] i1 = {1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50};
+        int[] o1 = {3500,9,10,70,2,3,11,0,99,30,40,50};
+        System.out.println(test_IntCode_one(i1, o1));
+
+        int[] i2 = {1,0,0,0,99};
+        int[] o2 = {2,0,0,0,99};
+        System.out.println(test_IntCode_one(i2, o2));
+
+        int[] i3 = {2,3,0,3,99};
+        int[] o3 = {2,3,0,6,99};
+        System.out.println(test_IntCode_one(i3, o3));
+
+        int[] i4 = {2,4,4,5,99,0};
+        int[] o4 = {2,4,4,5,99,9801};
+        System.out.println(test_IntCode_one(i4, o4));
+
+        int[] i5 = {1,1,1,4,99,5,6,0,99};
+        int[] o5 = {30,1,1,4,2,5,6,0,99};
+        System.out.println(test_IntCode_one(i5, o5));
     }
 }
