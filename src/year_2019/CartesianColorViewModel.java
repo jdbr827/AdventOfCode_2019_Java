cd79e04e9b1f37aafa99015ef91555ad629d22b5
Jake <reinhartjacob827@gmail.com>
2021-12-21 12:39:38 -0500

    refactored changes between CartesianViewModel and CartesianColorViewModel

--- src/year_2019/CartesianColorViewModel.java
+++ src/year_2019/CartesianColorViewModel.java
@@ -2,79 +2,17 @@ package year_2019;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Vector;
 
 public abstract class CartesianColorViewModel extends CartesianViewModel {
-    protected DefaultTableModel dtm = new DefaultTableModel(1, 1);
-    protected Map<Point, Color> cartesianColorMap = new HashMap<Point, Color>();
-
-    private void addNewPointIfNecessary(DefaultTableModel dtm, CartesianPoint desiredPointCartesian) {
-        Point desiredPointJava = convertCartesianToJava(desiredPointCartesian);
-
-        int Y = desiredPointJava.y;
-        int X = desiredPointJava.x;
-        while (Y < 0) {
-            Vector<Integer> newCol = new Vector<>();
-            for (int i = 0; i < dtm.getRowCount(); i++) {
-                newCol.add(-1);
-            }
-            DefaultTableModel newDTM = new DefaultTableModel(dtm.getRowCount(), dtm.getColumnCount() + 1);
-            Vector<Vector> oldDataVector = dtm.getDataVector();
-            for (Vector v : oldDataVector) {
-                v.insertElementAt(-1, 0);
-            }
-            Vector<Integer> newIdentifiers = new Vector<>();
-            for (int i = 0; i < newDTM.getColumnCount(); i++) {
-                newIdentifiers.add(i);
-            }
-            dtm.setDataVector(oldDataVector, newIdentifiers);
-            Y += 1;
-            cartesianOrigin.translate(1, 0);
-        }
-        while (Y >= dtm.getColumnCount()) {
-            Vector<Integer> newCol = new Vector<>();
-            for (int i = 0; i < dtm.getRowCount(); i++) {
-                newCol.add(-1);
-            }
-            dtm.addColumn(0, newCol);
-        }
-
-        while (X < 0) {
-            Vector<Integer> newRow = new Vector<>();
-            for (int i = 0; i < dtm.getColumnCount(); i++) {
-                newRow.add(-1);
-            }
-            dtm.insertRow(0, newRow);
-            X += 1;
-            cartesianOrigin.translate(0, 1);
-        }
-        while (X >= dtm.getRowCount()) {
-            Vector<Integer> newRow = new Vector<>();
-            for (int i = 0; i < dtm.getColumnCount(); i++) {
-                newRow.add(-1);
-            }
-            this.dtm.addRow(newRow);
-        }
-    }
+    protected Map<CartesianPoint, Color> cartesianColorMap = new HashMap<CartesianPoint, Color>();
 
     public void setColorAtCartesian(CartesianPoint desiredPointCartesian, Color color) {
         addNewPointIfNecessary(dtm, desiredPointCartesian);
         cartesianColorMap.put(desiredPointCartesian, color);
-    }
-
-    public void setValueAtCartesian(CartesianPoint droidLocation, Object value) {
-        addNewPointIfNecessary(dtm, droidLocation);
-        JavaPoint javaPoint = convertCartesianToJava(droidLocation);
-        dtm.setValueAt(value, javaPoint.x, javaPoint.y);
-    }
-
-     public void setModelToTable(JTable table1) {
-        table1.setModel(dtm);
     }
 
     public abstract Color getBackgroundColorAtCartesian(Point q);
--- src/year_2019/CartesianViewModel.java
+++ src/year_2019/CartesianViewModel.java
@@ -1,10 +1,13 @@
 package year_2019;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.Vector;
 
 public class CartesianViewModel {
     protected Point cartesianOrigin = new Point(0, 0);
+    protected DefaultTableModel dtm = new DefaultTableModel(1, 1);
 
     public JavaPoint convertCartesianToJava(CartesianPoint cartesianPoint) {
         return new JavaPoint(cartesianOrigin.y - cartesianPoint.y, cartesianPoint.x + cartesianOrigin.x);
@@ -14,6 +17,66 @@ public class CartesianViewModel {
         return new CartesianPoint(javaPoint.y - cartesianOrigin.y, javaPoint.x - cartesianOrigin.x);
     }
 
+    protected void addNewPointIfNecessary(DefaultTableModel dtm, CartesianPoint desiredPointCartesian) {
+        Point desiredPointJava = convertCartesianToJava(desiredPointCartesian);
 
+        addNewJavaPointIfNecessary(dtm, desiredPointJava);
+    }
 
+    private void addNewJavaPointIfNecessary(DefaultTableModel dtm, Point desiredPointJava) {
+        int Y = desiredPointJava.y;
+        int X = desiredPointJava.x;
+        while (Y < 0) {
+            Vector<Integer> newCol = new Vector<>();
+            for (int i = 0; i < dtm.getRowCount(); i++) {
+                newCol.add(-1);
+            }
+            DefaultTableModel newDTM = new DefaultTableModel(dtm.getRowCount(), dtm.getColumnCount() + 1);
+            Vector<Vector> oldDataVector = dtm.getDataVector();
+            for (Vector v : oldDataVector) {
+                v.insertElementAt(-1, 0);
+            }
+            Vector<Integer> newIdentifiers = new Vector<>();
+            for (int i = 0; i < newDTM.getColumnCount(); i++) {
+                newIdentifiers.add(i);
+            }
+            dtm.setDataVector(oldDataVector, newIdentifiers);
+            Y += 1;
+            cartesianOrigin.translate(1, 0);
+        }
+        while (Y >= dtm.getColumnCount()) {
+            Vector<Integer> newCol = new Vector<>();
+            for (int i = 0; i < dtm.getRowCount(); i++) {
+                newCol.add(-1);
+            }
+            dtm.addColumn(0, newCol);
+        }
+
+        while (X < 0) {
+            Vector<Integer> newRow = new Vector<>();
+            for (int i = 0; i < dtm.getColumnCount(); i++) {
+                newRow.add(-1);
+            }
+            dtm.insertRow(0, newRow);
+            X += 1;
+            cartesianOrigin.translate(0, 1);
+        }
+        while (X >= dtm.getRowCount()) {
+            Vector<Integer> newRow = new Vector<>();
+            for (int i = 0; i < dtm.getColumnCount(); i++) {
+                newRow.add(-1);
+            }
+            this.dtm.addRow(newRow);
+        }
+    }
+
+    public void setValueAtCartesian(CartesianPoint droidLocation, Object value) {
+        addNewPointIfNecessary(dtm, droidLocation);
+        JavaPoint javaPoint = convertCartesianToJava(droidLocation);
+        dtm.setValueAt(value, javaPoint.x, javaPoint.y);
+    }
+
+    public void setModelToTable(JTable table1) {
+        table1.setModel(dtm);
+    }
 }
