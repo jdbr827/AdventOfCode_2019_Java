c2ffd48cc3821f9cdac03185690eaa78ad8b8b35
Jake <reinhartjacob827@gmail.com>
2022-06-27 17:34:21 -0400

    D24 got one minute

--- src/year_2019/day24/GameOfBugLife.java
+++ src/year_2019/day24/GameOfBugLife.java
@@ -1,38 +1,88 @@
 package year_2019.day24;
 
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class GameOfBugLife {
-    private int[][] board;
+    private boolean[][] board;
 
-    GameOfBugLife(int[][] board){
+    GameOfBugLife(boolean[][] board) {
         this.board = board;
         System.out.println(Arrays.deepToString(this.board));
     }
 
-
-
+    /**
+     * Returns true iff new board is same as old board
+     *
+     * @return
+     */
+    public boolean runOneMinute() {
+        boolean[][] newBoard = new boolean[5][5];
+        boolean isSame = true;
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
+                newBoard[x][y] = determineNewState(x, y);
+                System.out.println(x + " " + y + " " + determineLiveNeighbors(x, y) + " " + newBoard[x][y]);
+                if (newBoard[x][y] != board[x][y]) {
+                    isSame = false;
+                }
+            }
+        }
+        this.board = newBoard;
+        return isSame;
+    }
 
 
     public static void main(String[] args) throws IOException {
         GameOfBugLife game = readInBoard();
+        game.runOneMinute();
+        System.out.println(Arrays.deepToString(game.board));
     }
+
     static GameOfBugLife readInBoard() throws IOException {
-        FileReader inputFileReader = new FileReader("./src/year_2019/day24/day24input.txt");
-        int[][] board = new int[5][5];
+        BufferedReader br = new BufferedReader(new FileReader("./src/year_2019/day24/day24input.txt"));
+        boolean[][] board = new boolean[5][5];
+        String line;
         for (int x = 0; x < 5; x++) {
+            line = br.readLine();
             for (int y = 0; y < 5; y++) {
-                char c = (char) inputFileReader.read();
+                char c = line.charAt(y);
                 System.out.println(x + " " + y + " " + c);
-                board[x][y] = (c == '#')? 1 : 0;
+                board[x][y] = (c == '#');
             }
-            inputFileReader.read(); // newline;
         }
         return new GameOfBugLife(board);
     }
 
 
+    private boolean determineNewState(int x, int y) {
+        boolean currentState = this.board[x][y];
+        int liveNeighbors = determineLiveNeighbors(x, y);
+        if (currentState) {
+            return liveNeighbors == 1;
+        } else {
+            return liveNeighbors == 1 || liveNeighbors == 2;
+        }
+    }
+
+    private int determineLiveNeighbors(int x, int y) {
+        int liveNeighbors = 0;
+        if (x > 0) {
+            liveNeighbors += this.board[x-1][y] ? 1 : 0;
+        }
+        if (x < 4) {
+            liveNeighbors += this.board[x + 1][y] ? 1 : 0;
+        }
+        if (y > 0) {
+            liveNeighbors += this.board[x][y-1] ? 1 : 0;
+        }
+        if (y < 4) {
+            liveNeighbors += this.board[x][y + 1] ? 1 : 0;
+        }
+        return liveNeighbors;
+    }
 }
