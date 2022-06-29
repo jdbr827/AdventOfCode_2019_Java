1317d88f93e285d7caed162a96b68d4ff318bd7b
Jake <reinhartjacob827@gmail.com>
2022-06-29 15:43:38 -0400

    D24 starting P2 first crack at counting neighbors (NOT TESTED)

--- src/year_2019/day24/InfiniteGameOfBugLife.java
+++ src/year_2019/day24/InfiniteGameOfBugLife.java
@@ -13,16 +13,16 @@ public class InfiniteGameOfBugLife {
     BugLifeLayer headLayer;
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         new InfiniteGameOfBugLife(readInBoard());
     }
 
-    InfiniteGameOfBugLife(Boolean[][] startingBoard) {
+    InfiniteGameOfBugLife(Boolean[][] startingBoard) throws Exception {
         headLayer = new BugLifeLayer(startingBoard);
         System.out.println(totalLiveNow());
     }
 
-    int totalLiveNow() {
+    int totalLiveNow() throws Exception {
         int total = 0;
         Optional<BugLifeLayer> layerPointer = Optional.of(headLayer);
         while (layerPointer.isPresent()) {
@@ -35,18 +35,32 @@ public class InfiniteGameOfBugLife {
             layerPointer = layerPointer.get().prev;
         }
         return total;
-
-
     };
 
 
 }
 
 class BugLifeLayer {
-    Boolean[][] board;
+    private Boolean[][] board;
     Optional<BugLifeLayer> next = Optional.empty();
     Optional<BugLifeLayer> prev = Optional.empty();
 
+    Boolean getValueAt(int x, int y) throws Exception {
+        if (x==2 && y==2) {
+            throw new Exception("Bug Life Layer has no value at middle!");
+        }
+        return board[x][y];
+
+
+    }
+
+    void setValueAt(int x, int y, Boolean b) throws Exception {
+        if (x==2 && y==2) {
+            throw new Exception("Bug Life Layer has no value at middle!");
+        }
+        board[x][y] = b;
+
+    }
     BugLifeLayer() {
         this(new Boolean[5][5]);
     }
@@ -73,13 +87,52 @@ class BugLifeLayer {
         return prev.get();
     }
 
-    public int totalLiveNow() {
+    public int totalLiveNow() throws Exception {
         int total = 0;
         for (int x=0; x<5; x++) {
             for (int y=0; y<5; y++) {
-                total += board[x][y] ? 1 : 0;
+                if (x == 2 && y == 2) {continue;}
+                total += getValueAt(x, y) ? 1 : 0;
             }
         }
         return total;
+    }
+
+    public int getLiveNeighbors(int x, int y) throws Exception {
+
+        int liveNeighbors = 0;
+        if (x == 0) {
+            liveNeighbors += getOrCreatePrev().getValueAt(2, 1) ? 1 : 0;
+        }
+        if (y == 0) {
+            liveNeighbors += getOrCreatePrev().getValueAt(1, 2) ? 1 : 0;
+        }
+        if (x == 4) {
+            liveNeighbors += getOrCreatePrev().getValueAt(3, 2) ? 1 : 0;
+        }
+        if (y == 4) {
+            liveNeighbors += getOrCreatePrev().getValueAt(2, 3) ? 1 : 0;
+        }
+        if (x == 2 && y == 1) {
+            for (int yLower = 0; yLower < 5 ; yLower++) {
+                liveNeighbors += getOrCreateNext().getValueAt(0, yLower) ? 1 : 0;
+            }
+        }
+        if (x == 2 && y == 3) {
+            for (int yLower = 0; yLower < 5 ; yLower++) {
+                liveNeighbors += getOrCreateNext().getValueAt(4, yLower) ? 1 : 0;
+            }
+        }
+        if (x == 1 && y == 2) {
+            for (int xLower = 0; xLower < 5 ; xLower++) {
+                liveNeighbors += getOrCreateNext().getValueAt(xLower, 9) ? 1 : 0;
+            }
+        }
+        if (x == 1 && y == 3) {
+            for (int xLower = 0; xLower < 5 ; xLower++) {
+                liveNeighbors += getOrCreateNext().getValueAt(xLower, 4) ? 1 : 0;
+            }
+        }
+        return liveNeighbors;
     }
 }
