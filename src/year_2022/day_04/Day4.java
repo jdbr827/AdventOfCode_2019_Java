703a8e0f06d6e04f6ee2f230fc3ba582d8860b65
Jake <reinhartjacob827@gmail.com>
2022-12-13 15:14:18 -0500

    2022 D4 refactor to make AssignmentPair

--- src/year_2022/day_04/Day4.java
+++ src/year_2022/day_04/Day4.java
@@ -1,33 +1,33 @@
 package year_2022.day_04;
 
 
+import jdk.nashorn.api.tree.AssignmentTree;
+
 import java.io.FileNotFoundException;
+import java.util.function.Predicate;
 
 public class Day4 {
 
-
-    public static int part1(String fileName) throws FileNotFoundException {
+    private static int countPairsWithProperty(String fileName, Predicate<Day4AssignmentPair> predicate) throws FileNotFoundException {
         Day4Scanner scanner = new Day4Scanner(fileName);
         Day4AssignmentPair assignmentPair;
         int num_contained = 0;
         while ((assignmentPair = scanner.getNextLine()) != null) {
-            if (assignmentPair.completelyContains()) {
+            if (predicate.test(assignmentPair)) {
                 num_contained += 1;
             }
         }
         return num_contained;
+
+    }
+
+
+    public static int part1(String fileName) throws FileNotFoundException {
+        return countPairsWithProperty(fileName, Day4AssignmentPair::completelyContains);
     }
 
      public static int part2(String fileName) throws FileNotFoundException {
-        Day4Scanner scanner = new Day4Scanner(fileName);
-        Day4AssignmentPair assignmentPair;
-        int num_contained = 0;
-        while ((assignmentPair = scanner.getNextLine()) != null) {
-           if (assignmentPair.hasOverlap()) {
-                num_contained += 1;
-            }
-        }
-        return num_contained;
+       return countPairsWithProperty(fileName, Day4AssignmentPair::hasOverlap);
     }
 
 
