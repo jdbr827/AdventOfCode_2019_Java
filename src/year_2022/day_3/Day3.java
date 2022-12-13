a6823bab0051b213f6d9e82d1205f69002c124d4
Jake <reinhartjacob827@gmail.com>
2022-12-13 13:21:55 -0500

    2022 D3 Part 1 correct

--- src/year_2022/day_3/Day3.java
+++ src/year_2022/day_3/Day3.java
@@ -1,6 +1,8 @@
 package year_2022.day_3;
 
 import java.io.FileNotFoundException;
+import java.security.InvalidKeyException;
+import java.security.KeyException;
 
 public class Day3 {
 
@@ -8,7 +10,7 @@ public class Day3 {
 
     }
 
-    public static int part1(String filename) throws FileNotFoundException {
+    public static int part1(String filename) throws FileNotFoundException, KeyException {
         Day3Scanner scanner = new Day3Scanner(filename);
         String thisLine;
         int totalMissing = 0;
@@ -19,18 +21,29 @@ public class Day3 {
         return totalMissing;
     }
 
-    protected static int getPriority(char thisChar) throws Exception {
+    protected static int getPriority(char thisChar) throws InvalidKeyException {
         int thisOrd = (int) thisChar;
         if (thisOrd > 96) {
             return thisOrd - 96;
         } else if (thisOrd > 64 && thisOrd < 91) {
             return thisOrd - 64 + 26;
         } else {
-            throw new Exception("Unrecognized character " + thisChar + " for which we tried to get priority");
+            throw new InvalidKeyException("Unrecognized character " + thisChar + " for which we tried to get priority");
         }
     }
 
-    private static int getMissingPriority(String thisLine) {
-        return 0;
+    protected static int getMissingPriority(String thisLine) throws KeyException {
+        boolean[] priorityPresent = new boolean[53]; // could optimize for off-by-1 later
+        int n = thisLine.length();
+        for (int i=0; i<n/2; i++) {
+            priorityPresent[getPriority(thisLine.charAt(i))] = true;
+        }
+        for (int i=n/2; i<n; i++) {
+            if (priorityPresent[getPriority(thisLine.charAt(i))]) {
+                return getPriority(thisLine.charAt(i));
+            }
+        }
+        throw new InvalidKeyException("No shared element found");
+        // execution should never reach here
     }
 }
--- src/year_2022/day_3/Day3Test.java
+++ src/year_2022/day_3/Day3Test.java
@@ -3,16 +3,19 @@ package year_2022.day_3;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+import java.security.KeyException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day3Test {
 
     public static String EXAMPLE_INPUT = "src/year_2022/day_3/day_3_sample_input.txt";
+    public static String OFFICIAL_INPUT = "src/year_2022/day_3/day_3_input.txt";
 
     @Test
-    public void test_scanner() throws FileNotFoundException {
-        Day3.part1(EXAMPLE_INPUT);
+    public void test_part1() throws FileNotFoundException, KeyException {
+        assertEquals(157, Day3.part1(EXAMPLE_INPUT));
+        assertEquals(7674, Day3.part1(OFFICIAL_INPUT));
     }
 
     @Test
@@ -23,5 +26,10 @@ public class Day3Test {
         assertEquals(27, Day3.getPriority('A'));
         assertEquals(36, Day3.getPriority('J'));
         assertEquals(52, Day3.getPriority('Z'));
+    }
+
+    @Test
+    public void test_missing() throws Exception {
+        assertEquals(16, Day3.getMissingPriority("vJrwpWtwJgWrhcsFMMfFFhFp"));
     }
 }
