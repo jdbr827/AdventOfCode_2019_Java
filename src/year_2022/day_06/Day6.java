1cbcb4ea094f5af9142d2f0439c11b36cdf4f48e
Jake <reinhartjacob827@gmail.com>
2022-12-19 12:31:19 -0500

    2022 D6 P2 methods 2 and 3

--- src/year_2022/day_06/Day6.java
+++ src/year_2022/day_06/Day6.java
@@ -1,20 +1,26 @@
 package year_2022.day_06;
 
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 
 public class Day6 {
+    Day6Scanner scanner;
 
+    public Day6(String fileName) throws FileNotFoundException {
+        scanner = new Day6Scanner(fileName);
+
+
+
+    }
 
     public static int part1(String fileName) throws FileNotFoundException {
-        return findStepsUntilLastNAllDiff(fileName, 4);
+        return findStepsUntilLastNAllDiffStatic(fileName, 4);
     }
 
     public static int part2(String fileName) throws FileNotFoundException {
-        return findStepsUntilLastNAllDiff(fileName, 14);
+        return findStepsUntilLastNAllDiffStatic(fileName, 14);
     }
 
-    protected static int findStepsUntilLastNAllDiff(String fileName, int N) throws FileNotFoundException {
+    protected static int findStepsUntilLastNAllDiffStatic(String fileName, int N) throws FileNotFoundException {
         Day6Scanner scanner = new Day6Scanner(fileName);
 
         Character[] previous = new Character[N];
@@ -44,10 +50,7 @@ public class Day6 {
         }
     }
 
-    protected static int findStepsUntilLastNAllDiffMethod2(String fileName, int N) throws FileNotFoundException {
-        Day6Scanner scanner = new Day6Scanner(fileName);
-
-
+    protected int findStepsUntilLastNAllDiffMethod2(int N) {
         Character[] previous = new Character[N];
         for (int i = 0; i < N; i++) {
             previous[N - 1 - i] = scanner.getNextChar();
@@ -56,16 +59,42 @@ public class Day6 {
         int minToScan = getMinToScan(previous);
 
         while (minToScan > 0) {
-            for (int i = N - 1; i > 0; i--) {
-                previous[i] = previous[i - 1];
-            }
-            previous[0] = scanner.getNextChar();
+            cyclePrevious(N, previous);
             minToScan -= 1;
 
-            minToScan = Math.max(minToScan, getMinToScan(previous));
+            for (int i=1; i<N-minToScan; i++) {
+                if (previous[i].equals(previous[0])) {
+                    minToScan = N-i;
+                }
+            }
         }
 
         return scanner.getNumScanned();
+    }
+
+    protected int findStepsUntilLastNAllDiffMethod3(int N) {
+        Character[] previous = new Character[N];
+        int minToScan = N;
+
+        while (minToScan > 0) {
+            cyclePrevious(N, previous);
+            minToScan -= 1;
+
+            for (int i=1; i<N-minToScan; i++) {
+                if (previous[i].equals(previous[0])) {
+                    minToScan = N-i;
+                }
+            }
+        }
+
+        return scanner.getNumScanned();
+    }
+
+    private void cyclePrevious(int N, Character[] previous) {
+        for (int i = N - 1; i > 0; i--) {
+            previous[i] = previous[i - 1];
+        }
+        previous[0] = scanner.getNextChar();
     }
 
     private static int getMinToScan(Character[] p) {
--- src/year_2022/day_06/Day6Test.java
+++ src/year_2022/day_06/Day6Test.java
@@ -40,19 +40,38 @@ public class Day6Test {
 
       @Test
       public void test_method2() throws FileNotFoundException {
-          assertEquals(7, Day6.findStepsUntilLastNAllDiffMethod2(EXAMPLE_INPUT_1, 4));
-          assertEquals(5, Day6.findStepsUntilLastNAllDiffMethod2(EXAMPLE_INPUT_2, 4));
-          assertEquals(6, Day6.findStepsUntilLastNAllDiffMethod2(EXAMPLE_INPUT_3, 4));
-          assertEquals(10, Day6.findStepsUntilLastNAllDiffMethod2(EXAMPLE_INPUT_4, 4));
-          assertEquals(11, Day6.findStepsUntilLastNAllDiffMethod2(EXAMPLE_INPUT_5, 4));
-          assertEquals(1850, Day6.findStepsUntilLastNAllDiffMethod2(OFFICIAL_INPUT, 4));
+          assertEquals(7, new Day6(EXAMPLE_INPUT_1).findStepsUntilLastNAllDiffMethod2(4));
+          assertEquals(5, new Day6(EXAMPLE_INPUT_2).findStepsUntilLastNAllDiffMethod2(4));
+          assertEquals(6, new Day6(EXAMPLE_INPUT_3).findStepsUntilLastNAllDiffMethod2(4));
+          assertEquals(10, new Day6(EXAMPLE_INPUT_4).findStepsUntilLastNAllDiffMethod2(4));
+          assertEquals(11, new Day6(EXAMPLE_INPUT_5).findStepsUntilLastNAllDiffMethod2(4));
+          assertEquals(1850, new Day6(OFFICIAL_INPUT).findStepsUntilLastNAllDiffMethod2( 4));
 
-          assertEquals(19, Day6.findStepsUntilLastNAllDiffMethod2(EXAMPLE_INPUT_1, 14));
-          assertEquals(23, Day6.findStepsUntilLastNAllDiffMethod2(EXAMPLE_INPUT_2, 14));
-          assertEquals(23, Day6.findStepsUntilLastNAllDiffMethod2(EXAMPLE_INPUT_3, 14));
-          assertEquals(29, Day6.findStepsUntilLastNAllDiffMethod2(EXAMPLE_INPUT_4, 14));
-          assertEquals(26, Day6.findStepsUntilLastNAllDiffMethod2(EXAMPLE_INPUT_5, 14));
-          assertEquals(2823, Day6.findStepsUntilLastNAllDiffMethod2(OFFICIAL_INPUT, 14));
+          assertEquals(19, new Day6(EXAMPLE_INPUT_1).findStepsUntilLastNAllDiffMethod2( 14));
+          assertEquals(23, new Day6(EXAMPLE_INPUT_2).findStepsUntilLastNAllDiffMethod2( 14));
+          assertEquals(23, new Day6(EXAMPLE_INPUT_3).findStepsUntilLastNAllDiffMethod2( 14));
+          assertEquals(29, new Day6(EXAMPLE_INPUT_4).findStepsUntilLastNAllDiffMethod2( 14));
+          assertEquals(26, new Day6(EXAMPLE_INPUT_5).findStepsUntilLastNAllDiffMethod2( 14));
+          assertEquals(2823, new Day6(OFFICIAL_INPUT).findStepsUntilLastNAllDiffMethod2( 14));
+
+      }
+
+
+       @Test
+      public void test_method3() throws FileNotFoundException {
+          assertEquals(7, new Day6(EXAMPLE_INPUT_1).findStepsUntilLastNAllDiffMethod3(4));
+          assertEquals(5, new Day6(EXAMPLE_INPUT_2).findStepsUntilLastNAllDiffMethod3(4));
+          assertEquals(6, new Day6(EXAMPLE_INPUT_3).findStepsUntilLastNAllDiffMethod3(4));
+          assertEquals(10, new Day6(EXAMPLE_INPUT_4).findStepsUntilLastNAllDiffMethod3(4));
+          assertEquals(11, new Day6(EXAMPLE_INPUT_5).findStepsUntilLastNAllDiffMethod3(4));
+          assertEquals(1850, new Day6(OFFICIAL_INPUT).findStepsUntilLastNAllDiffMethod3( 4));
+
+          assertEquals(19, new Day6(EXAMPLE_INPUT_1).findStepsUntilLastNAllDiffMethod3( 14));
+          assertEquals(23, new Day6(EXAMPLE_INPUT_2).findStepsUntilLastNAllDiffMethod3( 14));
+          assertEquals(23, new Day6(EXAMPLE_INPUT_3).findStepsUntilLastNAllDiffMethod3( 14));
+          assertEquals(29, new Day6(EXAMPLE_INPUT_4).findStepsUntilLastNAllDiffMethod3( 14));
+          assertEquals(26, new Day6(EXAMPLE_INPUT_5).findStepsUntilLastNAllDiffMethod3( 14));
+          assertEquals(2823, new Day6(OFFICIAL_INPUT).findStepsUntilLastNAllDiffMethod3( 14));
 
       }
 
