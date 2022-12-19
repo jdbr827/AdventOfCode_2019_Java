c1b592c52018a1ff70f70e608de566b718bc57b1
Jake <reinhartjacob827@gmail.com>
2022-12-19 15:43:49 -0500

    2022 D8 P2 correct

--- src/year_2022/day_08/Day8.java
+++ src/year_2022/day_08/Day8.java
@@ -2,6 +2,7 @@ package year_2022.day_08;
 
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -41,5 +42,30 @@ public class Day8 {
            }
        }
        return count;
+    }
+
+     public static long part2(String fileName) throws FileNotFoundException {
+       List<List<Integer>> trees = new Day8Scanner(fileName).readInMatrix();
+       int N = trees.size();
+       int M = trees.get(0).size();
+       long[][] scenicScore = new long[N][M];
+       long bestScenicScore = 0;
+       int count = 0;
+       for (int i=0; i<N; i++) {
+           for (int j=0; j<N; j++) {
+               int finalJ = j;
+               int finalI = i;
+               long up = IntStream.range(1, i+1).takeWhile(i_prime -> trees.get(finalI - i_prime).get(finalJ) < trees.get(finalI).get(finalJ)).count() + ((IntStream.range(0, i).allMatch((i_prime -> trees.get(i_prime).get(finalJ) < trees.get(finalI).get(finalJ))))? 0 : 1 );
+               //System.out.println(Arrays.toString(IntStream.range(1, i+1).takeWhile(i_prime -> trees.get(finalI - i_prime).get(finalJ) < trees.get(finalI).get(finalJ)).toArray()));
+               long down = IntStream.range(i+1, N).takeWhile(i_prime -> trees.get(i_prime).get(finalJ) < trees.get(finalI).get(finalJ)).count() + ((IntStream.range(i+1, N).allMatch((i_prime -> trees.get(i_prime).get(finalJ) < trees.get(finalI).get(finalJ)))) ? 0 : 1);
+               long left = IntStream.range(1, j+1).takeWhile(j_prime -> trees.get(finalI).get(finalJ - j_prime) < trees.get(finalI).get(finalJ)).count() + ((IntStream.range(0, j).allMatch((j_prime -> trees.get(finalI).get(j_prime) < trees.get(finalI).get(finalJ)))) ? 0 : 1);
+               long right = IntStream.range(j+1, M).takeWhile(j_prime -> trees.get(finalI).get(j_prime) < trees.get(finalI).get(finalJ)).count() + ((IntStream.range(j+1, M).allMatch((j_prime -> trees.get(finalI).get(j_prime) < trees.get(finalI).get(finalJ)))) ? 0 : 1);
+               //System.out.println(i + " " + j + " " + up + " " + down + " " + left + " " + right);
+               scenicScore[finalI][finalJ] = up * down * left * right;
+               bestScenicScore = Math.max(bestScenicScore, scenicScore[i][j]);
+
+           }
+       }
+       return bestScenicScore;
     }
 }
--- src/year_2022/day_08/Day8Test.java
+++ src/year_2022/day_08/Day8Test.java
@@ -3,6 +3,7 @@ package year_2022.day_08;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,6 +17,13 @@ public class Day8Test {
     public void test_part1() throws FileNotFoundException {
         assertEquals(21, Day8.part1(EXAMPLE_INPUT));
         assertEquals(1792, Day8.part1(OFFICIAL_INPUT));
+
+    }
+
+    @Test
+    public void test_part2() throws FileNotFoundException {
+        assertEquals(8, Day8.part2(EXAMPLE_INPUT));
+        assertEquals(334880, Day8.part2(OFFICIAL_INPUT));
 
     }
 }
