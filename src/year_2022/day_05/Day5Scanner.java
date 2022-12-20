8c7a7a8ccf72856609bb5983c940721dd75ad368
Jake <reinhartjacob827@gmail.com>
2022-12-20 11:36:05 -0500

    2022 D5 clean up scanner

--- src/year_2022/day_05/Day5Scanner.java
+++ src/year_2022/day_05/Day5Scanner.java
@@ -1,6 +1,7 @@
 package year_2022.day_05;
 
 import com.google.common.base.Splitter;
+import utils.AOCScanner;
 import utils.ReadIn;
 
 import java.io.File;
@@ -10,36 +11,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class Day5Scanner {
-    Scanner scanner;
+public class Day5Scanner extends AOCScanner {
 
-    static final Pattern cratesPattern = Pattern.compile("\\[([A-Z])]");
-    static final Pattern numbersPattern = Pattern.compile("\\s([0-9]+)\\s");
     static final Pattern instructionsPattern = Pattern.compile("move ([0-9]+) from ([0-9]+) to ([0-9]+)");
 
     public Day5Scanner(String fileName) throws FileNotFoundException {
-        File file = new File(fileName);
-        scanner = new Scanner(file);
-    }
-
-    public List<Character> getNextLine() {
-        String line = scanner.nextLine();
-        //System.out.println(line);
-        List<String> splitLine = Splitter.fixedLength(4).splitToList(line);
-        //System.out.println(splitLine.get(0).charAt(1));
-        if (splitLine.get(0).charAt(1) == '1') {
-            return List.of();
-        }
-
-        return splitLine.stream().map((crate) -> {
-            if (crate.trim().isEmpty()) {
-                return '7';
-            } else {
-                Matcher m = cratesPattern.matcher(crate);
-                ReadIn.findOrElseThrow(m, "Could not match ranges pattern");
-                return m.group(1).charAt(0);
-            }
-        }).collect(Collectors.toList());
+        super(fileName);
     }
 
     public Day5 createStacks() {
