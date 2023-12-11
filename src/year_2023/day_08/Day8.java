970409c34ceee2cdaef255730f0b675d983b762c
Jake <reinhartjacob827@gmail.com>
2023-12-11 14:17:55 -0500

    2023 Day 8 Part 2 right but slow (3 min 40 sec)

--- src/year_2023/day_08/Day8.java
+++ src/year_2023/day_08/Day8.java
@@ -1,8 +1,11 @@
 package year_2023.day_08;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-import java.util.Map;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 public class Day8 {
@@ -19,5 +22,104 @@ public class Day8 {
             nSteps++;
         }
         return nSteps;
+    }
+
+
+     int countStepsToZZZGhost2() {
+        List<String> location = desertMap.keySet().stream().filter(key -> key.charAt(2) == 'A').collect(Collectors.toList());
+        int instructionIdx = 0;
+        int nSteps = 0;
+        Predicate<String> endCondition = key -> key.charAt(2) == 'Z';
+        while (!location.stream().allMatch(endCondition)) {
+            Character instruction = instructions.charAt(instructionIdx);
+            location = location.stream().map(l -> desertMap.get(l).get(instruction)).collect(Collectors.toList());
+            instructionIdx = (instructionIdx + 1) % instructions.length();
+            nSteps++;
+        }
+        return nSteps;
+    }
+
+
+    class DesertGhost {
+        String startLocation;
+        int period;
+        List<Integer> offsetsWhereYouEndAtZ = new ArrayList<>();
+        int cycleStartStep;
+
+        @Getter
+        long lowestPossibleSolution = 0;
+
+
+        int offsetPointer = 0;
+        int timesPeriodUsed = 0;
+
+        DesertGhost(String startLocation) {
+            this.startLocation = startLocation;
+            determineCycleInfo();
+        }
+
+        void determineCycleInfo() {
+            List<String> locationsVisited = new java.util.ArrayList<>();
+            String currentLocation = startLocation;
+            Map<String, Map<Integer, Integer>> visitedLocationOnInstructionAtStep = new HashMap<>();
+            int instructionIdx = 0;
+            int nSteps = 0;
+            while (!visitedLocationOnInstructionAtStep.getOrDefault(currentLocation, new HashMap<>()).containsKey(instructionIdx)) {
+                visitedLocationOnInstructionAtStep.putIfAbsent(currentLocation, new HashMap<>());
+                visitedLocationOnInstructionAtStep.get(currentLocation).put(instructionIdx, nSteps);
+                locationsVisited.add(currentLocation);
+                currentLocation = desertMap.get(currentLocation).get(instructions.charAt(instructionIdx));
+                instructionIdx++;
+                if (instructionIdx >= instructions.length()) {
+                    instructionIdx = 0;
+                }
+                nSteps++;
+            }
+
+            this.cycleStartStep = visitedLocationOnInstructionAtStep.get(currentLocation).get(instructionIdx);
+            this.period = nSteps - cycleStartStep;
+
+            List<Boolean> isOffsetLocationEndWithZ = locationsVisited.subList(cycleStartStep, cycleStartStep + period).stream().map(key -> key.charAt(2) == 'Z').collect(Collectors.toList());
+            for (int i=0; i<period; i++) {
+                if (isOffsetLocationEndWithZ.get(i)) {
+                    offsetsWhereYouEndAtZ.add(i);
+                }
+            }
+
+            lowestPossibleSolution = cycleStartStep + (long) timesPeriodUsed * period + offsetsWhereYouEndAtZ.get(offsetPointer);
+        }
+
+
+        void computeLowestPossibleSolution() {
+          lowestPossibleSolution = cycleStartStep + (long) timesPeriodUsed * period + offsetsWhereYouEndAtZ.get(offsetPointer);
+        }
+
+        void updateLowestPossibleSolution() {
+            offsetPointer++;
+            if (offsetPointer >= offsetsWhereYouEndAtZ.size()) {
+                timesPeriodUsed += 1;
+                offsetPointer = 0;
+            }
+            computeLowestPossibleSolution();
+        }
+    }
+
+
+       long countStepsToZZZGhost() {
+        Collection<DesertGhost> ghosts = desertMap.keySet().stream()
+                .filter(key -> key.charAt(2) == 'A')
+                .map(DesertGhost::new)
+                .collect(Collectors.toList());
+
+        while (true) {
+            DesertGhost smallestGhost = ghosts.stream().min(Comparator.comparing(DesertGhost::getLowestPossibleSolution)).get();
+            long largestLowest = ghosts.stream().map(DesertGhost::getLowestPossibleSolution).max(Comparator.naturalOrder()).get();
+            if (smallestGhost.getLowestPossibleSolution() == largestLowest) {
+                return smallestGhost.getLowestPossibleSolution();
+            }
+            while (smallestGhost.getLowestPossibleSolution() < largestLowest) {
+                smallestGhost.updateLowestPossibleSolution();
+            }
+        }
     }
 }
--- src/year_2023/day_08/Day8Test.java
+++ src/year_2023/day_08/Day8Test.java
@@ -9,12 +9,21 @@ public class Day8Test {
     static final String SMALL_INPUT = "src/year_2023/day_08/day_8_2023_small_input.txt";
     static final String MEDIUM_INPUT = "src/year_2023/day_08/day_8_2023_medium_input.txt";
     static final String INPUT = "src/year_2023/day_08/day_8_2023_input.txt";
+    static final String PART_2_SMALL_INPUT = "src/year_2023/day_08/day_8_2023_part_2_small_input.txt";
 
     @Test
-    public void test_Scanner() {
+    public void test_part_1() {
         assertEquals(2, Day8Scanner.scan(SMALL_INPUT).countStepsToZZZ());
         assertEquals(6, Day8Scanner.scan(MEDIUM_INPUT).countStepsToZZZ());
         assertEquals(19637, Day8Scanner.scan(INPUT).countStepsToZZZ());
+
+    }
+
+
+     @Test
+    public void test_part_2() {
+        assertEquals(6L, Day8Scanner.scan(PART_2_SMALL_INPUT).countStepsToZZZGhost());
+        assertEquals(8811050362409L, Day8Scanner.scan(INPUT).countStepsToZZZGhost());
 
     }
 }
