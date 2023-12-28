4e68f669338720216f5618d3a08eba89b324c916
Jake <reinhartjacob827@gmail.com>
2023-12-27 20:26:51 -0500

    2023 Day 19 part 1 complete

--- src/year_2023/day_19/Day19Test.java
+++ src/year_2023/day_19/Day19Test.java
@@ -7,10 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Day19Test {
 
     public static final String SMALL_INPUT = "src/year_2023/day_19/day_19_2023_small_input.txt";
+    public static final String INPUT = "src/year_2023/day_19/day_19_2023_input.txt";
 
     @Test
     public void test_part1() {
         assertEquals(19114, new Day19(SMALL_INPUT).sumRatingNumbersOfAcceptedMachineParts());
+        assertEquals(449531, new Day19(INPUT).sumRatingNumbersOfAcceptedMachineParts());
 
     }
 }
