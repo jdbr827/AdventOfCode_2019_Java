4e547684f4d389abf21e4e33215b608de9bce19b
Jake <reinhartjacob827@gmail.com>
2024-01-05 13:53:14 -0500

    2023 Day 19 Part 2 moved MachinePartSpace to its own class

--- src/year_2023/day_19/Day19.java
+++ src/year_2023/day_19/Day19.java
@@ -2,7 +2,6 @@ package year_2023.day_19;
 
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import utils.AOCScanner;
 
 import java.util.*;
@@ -233,74 +232,5 @@ public class Day19 {
         }
     }
 
-    @Data
-    static class MachinePartSpace {
-        MachinePartSpaceDimension x;
-        MachinePartSpaceDimension m;
-        MachinePartSpaceDimension a;
-        MachinePartSpaceDimension s;
-        String currentWorkflow;
 
-        public MachinePartSpace(int xMin, int xMax, int mMin, int mMax, int aMin, int aMax, int sMin, int sMax, String currentWorkflow) {
-            x = new MachinePartSpaceDimension(xMin, xMax);
-            m = new MachinePartSpaceDimension(mMin, mMax);
-            a = new MachinePartSpaceDimension(aMin, aMax);
-            s = new MachinePartSpaceDimension(sMin, sMax);
-            this.currentWorkflow = currentWorkflow;
-        }
-
-
-        private MachinePartSpaceDimension getField(String fieldName) {
-            switch (fieldName) {
-                case "x":
-                    return x;
-                case "m":
-                    return m;
-                case "a":
-                    return a;
-                case "s":
-                    return s;
-            }
-            throw new Error("Did not recognize field name \"" + fieldName + "\". fieldName should only be one of x, m, a, s");
-        }
-
-        private void setMax(String fieldName, int i) {
-            getField(fieldName).setMaxInclusive(i);
-        }
-
-        private void setMin(String fieldName, int i) {
-            getField(fieldName).setMinInclusive(i);
-        }
-
-        private int getMax(String fieldName) {
-            return getField(fieldName).getMaxInclusive();
-        }
-
-        private int getMin(String fieldName) {
-            return getField(fieldName).getMinInclusive();
-        }
-
-        private MachinePartSpace copy() {
-            return new MachinePartSpace(x.minInclusive, x.maxInclusive, m.minInclusive, m.maxInclusive, a.minInclusive, a.maxInclusive, s.minInclusive, s.maxInclusive, currentWorkflow);
-        }
-
-
-
-
-        public long getArea() {
-            return x.getLength() * m.getLength() * a.getLength() * s.getLength();
-        }
-    }
-
-
-    @Data
-    @AllArgsConstructor
-    static class MachinePartSpaceDimension {
-        int minInclusive;
-        int maxInclusive;
-
-        public long getLength() {
-            return maxInclusive - minInclusive + 1;
-        }
-    }
 }
---
+++ src/year_2023/day_19/MachinePartSpace.java
@@ -0,0 +1,72 @@
+package year_2023.day_19;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+class MachinePartSpace {
+    private MachinePartSpaceDimension x;
+    private MachinePartSpaceDimension m;
+    private MachinePartSpaceDimension a;
+    private MachinePartSpaceDimension s;
+    String currentWorkflow;
+
+    public MachinePartSpace(int xMin, int xMax, int mMin, int mMax, int aMin, int aMax, int sMin, int sMax, String currentWorkflow) {
+        x = new MachinePartSpaceDimension(xMin, xMax);
+        m = new MachinePartSpaceDimension(mMin, mMax);
+        a = new MachinePartSpaceDimension(aMin, aMax);
+        s = new MachinePartSpaceDimension(sMin, sMax);
+        this.currentWorkflow = currentWorkflow;
+    }
+
+
+    private MachinePartSpaceDimension getField(String fieldName) {
+        switch (fieldName) {
+            case "x":
+                return x;
+            case "m":
+                return m;
+            case "a":
+                return a;
+            case "s":
+                return s;
+        }
+        throw new Error("Did not recognize field name \"" + fieldName + "\". fieldName should only be one of x, m, a, s");
+    }
+
+    public void setMax(String fieldName, int i) {
+        getField(fieldName).setMaxInclusive(i);
+    }
+
+    public void setMin(String fieldName, int i) {
+        getField(fieldName).setMinInclusive(i);
+    }
+
+    public int getMax(String fieldName) {
+        return getField(fieldName).getMaxInclusive();
+    }
+
+    public int getMin(String fieldName) {
+        return getField(fieldName).getMinInclusive();
+    }
+
+    public MachinePartSpace copy() {
+        return new MachinePartSpace(x.minInclusive, x.maxInclusive, m.minInclusive, m.maxInclusive, a.minInclusive, a.maxInclusive, s.minInclusive, s.maxInclusive, currentWorkflow);
+    }
+
+
+    public long getArea() {
+        return x.getLength() * m.getLength() * a.getLength() * s.getLength();
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MachinePartSpaceDimension {
+        int minInclusive;
+        int maxInclusive;
+
+        long getLength() {
+            return maxInclusive - minInclusive + 1;
+        }
+    }
+}
