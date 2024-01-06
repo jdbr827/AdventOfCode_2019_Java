a21cf2e4c1320def5317e22ba6f59cc43b3b64eb
Jake <reinhartjacob827@gmail.com>
2024-01-06 14:22:26 -0500

    2023 Day 21 Part 1 complete

--- src/year_2023/day_21/Day21.java
+++ src/year_2023/day_21/Day21.java
@@ -1,13 +1,67 @@
 package year_2023.day_21;
 
+import javafx.util.Pair;
+import utils.AOCScanner;
+import utils.BFSUtil;
+import viewModelUtil.CartesianPoint;
+import year_2019.day15.model.CardinalDirection;
+
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Day21 {
+    Character[][] garden;
 
 
     public Day21(String fileName) {
-
+        AOCScanner scanner = new AOCScanner(fileName);
+        garden = scanner.scanAsChar2DArray();
     }
 
-    public int getSteps(int numSteps) {
-        return 0;
+
+    private CartesianPoint findOrigin() {
+        int N = garden.length;
+        int M = garden[0].length;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (garden[i][j] == 'S') {
+                    return new CartesianPoint(i, j);
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public long getSteps(long numSteps) {
+        int N = garden.length;
+        int M = garden[0].length;
+
+        //find origin
+        CartesianPoint origin = findOrigin();
+
+          Function<CartesianPoint, Collection<CartesianPoint>> neighborFunction = (p ->
+                Arrays.stream(CardinalDirection.values())
+                        .map(d -> p.add(d.velocity))
+                        .filter((nbr) -> nbr.isInBoundariesInclusive(0, N - 1, 0, M - 1) && garden[nbr.x][nbr.y] != '#')
+                        .collect(Collectors.toList()));
+
+        List<CartesianPoint> pointsIStepsAway = List.of(origin);
+        List<CartesianPoint> pointsIPlusOneStepsAway = new ArrayList<>();
+        for (int i=0; i<numSteps; i++) {
+            pointsIPlusOneStepsAway = new ArrayList<>(pointsIStepsAway.stream()
+                    .map(neighborFunction)
+                    .reduce(new HashSet<>(), (acc, elm) -> {
+                        acc.addAll(elm);
+                        return acc;
+                    }));
+
+            pointsIStepsAway = pointsIPlusOneStepsAway;
+        }
+
+        return pointsIStepsAway.size();
     }
 }
+
--- src/year_2023/day_21/Day21Test.java
+++ src/year_2023/day_21/Day21Test.java
@@ -6,14 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day21Test {
 
-    public static final String SMALL_INPUT = "src/year_2023/day_21/day_20_2023_small_input.txt";
+    public static final String SMALL_INPUT = "src/year_2023/day_21/day_21_2023_small_input.txt";
     public static final String INPUT = "src/year_2023/day_21/day_21_2023_input.txt";
 
     @Test
     public void test_part1() {
         assertEquals(16, new Day21(SMALL_INPUT).getSteps(6));
-        System.out.println(new Day21(INPUT).getSteps(64));
-        //assertEquals(788848550, new Day21(INPUT).getSteps(64));
+        assertEquals(3724, new Day21(INPUT).getSteps(64));
     }
 
 }
