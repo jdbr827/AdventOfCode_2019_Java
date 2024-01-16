7bb333b40dbcf224c8b9f924807939d29a312bb5
Jake <reinhartjacob827@gmail.com>
2024-01-16 14:33:08 -0500

    2023 Day 22 Part 1 changed brick constructor

--- src/year_2023/day_22/Day22.java
+++ src/year_2023/day_22/Day22.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Day22 {
-    private static final Brick GROUND = new Brick(0, Integer.MAX_VALUE, 0, Integer.MAX_VALUE, 0, 0);
+    private static final Brick GROUND = new Brick(List.of(0, 0, 0), List.of(Integer.MAX_VALUE, Integer.MAX_VALUE, 0));
     Collection<Brick> bricks = new ArrayList<>();
      int xMax;
      int yMax;
@@ -65,7 +65,7 @@ public class Day22 {
         }
     }
 
-    @RequiredArgsConstructor
+
     public static class Brick {
         public List<Brick> supportedBy = new ArrayList<>();
         public List<Brick> supporting = new ArrayList<>();
@@ -76,12 +76,21 @@ public class Day22 {
         @NonNull Integer y2;
         @NonNull Integer z2;
 
+        Brick(List<Integer> p1, List<Integer> p2) {
+            x1 = p1.get(0);
+            y1 = p1.get(1);
+            z1 = p1.get(2);
+            x2 = p2.get(0);
+            y2 = p2.get(1);
+            z2 = p2.get(2);
+        }
+
         static Brick scanIn(String line) {
             String[] splitLine = line.split("~");
             String point1 = splitLine[0];
             List<Integer> p1 = Arrays.stream(point1.split(",")).map(Integer::parseInt).collect(Collectors.toList());
             List<Integer> p2 = Arrays.stream(splitLine[1].split(",")).map(Integer::parseInt).collect(Collectors.toList());
-            return new Brick(p1.get(0), p1.get(1), p1.get(2), p2.get(0), p2.get(1), p2.get(2));
+            return new Brick(p1, p2);
         }
 
         public int lowestZ() {
