fb34a8d26cf6c9253b8c665414a155cc08ec485f
jake <reinhartjacob827@gmail.com>
2024-12-20 15:03:53 -0500

    2024 Day 4 Part 1

---
+++ src/year_2024/day_04/Day4.java
@@ -0,0 +1,46 @@
+package year_2024.day_04;
+
+import org.testng.internal.collections.Pair;
+import utils.AOCScanner;
+
+import java.util.List;
+
+public class Day4 {
+    public static final List<Pair<Integer, Integer>> velocities = List.of(Pair.of(1, 0), Pair.of(1, 1), Pair.of(0, 1), Pair.of(-1, 1), Pair.of(-1, 0), Pair.of(-1, -1), Pair.of(0, -1), Pair.of(1, -1));
+    public static final String XMAS = "XMAS";
+
+    Character[][] grid;
+    int n;
+    int m;
+
+    public Day4(String smallInputFilename) {
+        grid = new AOCScanner(smallInputFilename).scanAsChar2DArray();
+        n = grid.length;
+        m = grid[0].length;
+    }
+
+
+    public int timesXMASAppears() {
+        int appearances = 0;
+        for (int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if (grid[i][j] == 'X') {
+                    for (Pair<Integer, Integer> velocity: velocities) {
+                        int i_star = i;
+                        int j_star = j;
+                        int k;
+                        for(k=1; k<4; k++) {
+                            i_star += velocity.first();
+                            j_star += velocity.second();
+                            if (i_star < 0 || i_star >= n || j_star < 0 || j_star >= m || grid[i_star][j_star] != XMAS.charAt(k)) {
+                                break;
+                            }
+                        }
+                        if (k == 4) {appearances += 1;}
+                    }
+                }
+            }
+        }
+        return appearances;
+    }
+}
---
+++ src/year_2024/day_04/Day4Test.java
@@ -0,0 +1,18 @@
+package year_2024.day_04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class Day4Test {
+
+    public static final String INPUT_FILENAME = "src/year_2024/day_04/input_aoc_2024_4.txt";
+    public static final String SMALL_INPUT_FILENAME = "src/year_2024/day_04/input_aoc_2024_4_small.txt";
+
+    @Test
+    public void test_part1() {
+        assertEquals(18, new Day4(SMALL_INPUT_FILENAME).timesXMASAppears());
+        assertEquals(2500, new Day4(INPUT_FILENAME).timesXMASAppears());
+    }
+
+}
