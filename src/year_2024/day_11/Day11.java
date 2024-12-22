d85a8b1edefd89ce4b08e6e8e6352952cdbc6fee
jake <reinhartjacob827@gmail.com>
2024-12-22 17:12:20 -0500

    2024 Day 11 Part 2 ideas too slow

--- src/year_2024/day_11/Day11.java
+++ src/year_2024/day_11/Day11.java
@@ -1,11 +1,14 @@
 package year_2024.day_11;
 
+import com.ea.async.Async;
 import utils.AOCScanner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class Day11 {
 
@@ -14,29 +17,48 @@ public class Day11 {
     public Day11(String inputFilename) {
         stones = Arrays.stream(new AOCScanner(inputFilename).nextLine().split("\\s")).map(Long::parseLong).toList();
     }
-
-    public long numStonesAfterBlinkingNTimes(int nTimes) {
-        for (int i=0; i<nTimes; i++) {
-            blink();
-        }
-        return stones.size();
+    public static List<Long> readIn(String inputFilename) {
+        return Arrays.stream(new AOCScanner(inputFilename).nextLine().split("\\s")).map(Long::parseLong).toList();
     }
 
-    private void blink() {
-        List<Long> newStones = new LinkedList<>();
-        for (Long stone : stones) {
+    public static long numStonesAfterBlinkingNTimes(List<Long> stones, int nTimes) {
+      return stones.stream().parallel().map(stone ->
+          numStonesAfterBlinkingNTimes(stone, nTimes)
+      ).reduce(0L, Math::addExact);
+    }
+
+    public static long numStonesAfterBlinkingNTimes(Long stone, int nTimes) {
+        if (nTimes == 0L) {
+            return 1L;
+        }
+        if (stone == 0L) {
+            return numStonesAfterBlinkingNTimes(1L, nTimes - 1);
+        }
+        String s = stone.toString();
+        if (s.length() % 2 == 0L) {
+            long left = numStonesAfterBlinkingNTimes(Long.parseLong(s.substring(0, s.length()/2)), nTimes - 1);
+            long right = numStonesAfterBlinkingNTimes(Long.parseLong(s.substring(s.length() / 2)), nTimes - 1);
+            return left + right;
+        } else {
+            return numStonesAfterBlinkingNTimes(stone * 2024, nTimes - 1);
+        }
+    }
+
+
+    private static List<Long> blink(List<Long> stones) {
+        return stones.stream().map((Long stone) -> {
             if (stone == 0L) {
-                newStones.add(1L);
-                continue;
+                return List.of(1L);
             }
             String s = stone.toString();
             if (s.length() % 2 == 0L) {
-                newStones.add(Long.parseLong(s.substring(0, s.length() / 2)));
-                newStones.add(Long.parseLong(s.substring(s.length() / 2)));
+                return List.of(
+                        Long.parseLong(s.substring(0, s.length() / 2)),
+                        Long.parseLong(s.substring(s.length() / 2))
+                );
             } else {
-                newStones.add(stone * 2024);
+                return List.of(stone * 2024);
             }
-        }
-        stones = newStones;
+        }).reduce(new LinkedList<>(), (ag, l) -> {ag.addAll(l); return ag;});
     }
 }
--- src/year_2024/day_11/Day11Test.java
+++ src/year_2024/day_11/Day11Test.java
@@ -2,6 +2,8 @@ package year_2024.day_11;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.testng.AssertJUnit.assertEquals;
 
 public class Day11Test {
@@ -11,8 +13,16 @@ public class Day11Test {
 
     @Test
     public void test_part1() {
-        assertEquals(22, new Day11(SMALL_INPUT_FILENAME).numStonesAfterBlinkingNTimes(6));
-        assertEquals(55312, new Day11(SMALL_INPUT_FILENAME).numStonesAfterBlinkingNTimes(25));
-        assertEquals(187738, new Day11(INPUT_FILENAME).numStonesAfterBlinkingNTimes(25));
+        assertEquals(22, Day11.numStonesAfterBlinkingNTimes(Day11.readIn(SMALL_INPUT_FILENAME), 6));
+        assertEquals(55312, Day11.numStonesAfterBlinkingNTimes(Day11.readIn(SMALL_INPUT_FILENAME), 25));
+        assertEquals(187738, Day11.numStonesAfterBlinkingNTimes(Day11.readIn(INPUT_FILENAME), 25));
+    }
+
+    @Test void test_part2() {
+        System.out.println(Day11.numStonesAfterBlinkingNTimes(List.of(1L), 75));
+        System.out.println(Day11.numStonesAfterBlinkingNTimes(Day11.readIn(SMALL_INPUT_FILENAME), 75));
+
+        //System.out.println(new Day11(SMALL_INPUT_FILENAME).numStonesAfterBlinkingNTimes(75));
+        //System.out.println(new Day11(INPUT_FILENAME).numStonesAfterBlinkingNTimes(75));
     }
 }
