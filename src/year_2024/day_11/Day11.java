a99d85e940e5913cf11b181c5701c126a9367b7e
jake <reinhartjacob827@gmail.com>
2024-12-22 17:59:23 -0500

    2024 Day 11 Part 2 cleanup

--- src/year_2024/day_11/Day11.java
+++ src/year_2024/day_11/Day11.java
@@ -9,21 +9,17 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class Day11 {
-    Map<Long, Long> stones = new HashMap<>();
 
-    public Day11(String inputFilename) {
-        List<Long> stonesList = Arrays.stream(new AOCScanner(inputFilename).nextLine().split("\\s")).map(Long::parseLong).toList();
-        stones = FrequencyTableUtil.createFrequencyTableLong(stonesList);
-    }
     public static List<Long> readIn(String inputFilename) {
         return Arrays.stream(new AOCScanner(inputFilename).nextLine().split("\\s")).map(Long::parseLong).toList();
     }
 
     public static long numStonesAfterBlinkingNTimes(String inputFilename, int nTimes) {
-        return new Day11(inputFilename).numStonesAfterBlinkingNTimes(nTimes);
+        return numStonesAfterBlinkingNTimes(Day11.readIn(inputFilename), nTimes);
     }
 
-    public long numStonesAfterBlinkingNTimes(int nTimes) {
+    public static long numStonesAfterBlinkingNTimes(List<Long> initialStones, int nTimes) {
+        Map<Long, Long> stones = FrequencyTableUtil.createFrequencyTableLong(initialStones);
         for (int i=0; i<nTimes; i++) {
             Map<Long, Long> newStones = new HashMap<>();
             for (Long stone : stones.keySet()) {
@@ -34,45 +30,6 @@ public class Day11 {
             stones = newStones;
         }
         return stones.values().stream().reduce(0L, Math::addExact);
-    }
-
-
-    public static long numStonesAfterBlinkingNTimes(Long stone, int nTimes) throws ExecutionException, InterruptedException {
-        long tot = 1L;
-        List<CompletableFuture<Long>> awaiting = new ArrayList<>();
-        //System.out.println(stone + " " + nTimes);
-        for (int i=0; i<nTimes; i++) {
-            if (stone == 0L) {
-                stone=1L;
-                continue;
-            }
-            String s = stone.toString();
-            if (s.length() % 2 == 0L) {
-                long leftNum = Long.parseLong(s.substring(0, s.length() / 2));
-                long rightNum = Long.parseLong(s.substring(s.length() / 2));
-                stone = leftNum;
-                int finalI = i;
-                awaiting.add(CompletableFuture.supplyAsync(() -> {
-                    try {
-                        return numStonesAfterBlinkingNTimes(rightNum, nTimes - finalI -1);
-                    } catch (ExecutionException | InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }));
-            } else {
-                stone *= 2024;
-            }
-        }
-        //System.out.println(stone);
-        for (CompletableFuture<Long> cf : awaiting) {
-            tot += cf.get();
-        }
-        return tot;
-    }
-
-
-    private static List<Long> blinkLst(List<Long> stones) {
-        return stones.stream().map(Day11::blink).reduce(new LinkedList<>(), (ag, l) -> {ag.addAll(l); return ag;});
     }
 
     private static @NotNull List<Long> blink(Long stone) {
--- src/year_2024/day_11/Day11Test.java
+++ src/year_2024/day_11/Day11Test.java
@@ -15,10 +15,10 @@ public class Day11Test {
     @Test
     public void test_part1() throws ExecutionException, InterruptedException {
         //assertEquals(7, Day11.numStonesAfterBlinkingNTimes(125L, 6));
-        assertEquals(1, Day11.numStonesAfterBlinkingNTimes(1L, 1));
-        assertEquals(6, Day11.numStonesAfterBlinkingNTimes(17L, 4));
-        assertEquals(8, Day11.numStonesAfterBlinkingNTimes(17L, 5));
-        assertEquals(15, Day11.numStonesAfterBlinkingNTimes(17L, 6));
+        assertEquals(1, Day11.numStonesAfterBlinkingNTimes(List.of(1L), 1));
+        assertEquals(6, Day11.numStonesAfterBlinkingNTimes(List.of(17L), 4));
+        assertEquals(8, Day11.numStonesAfterBlinkingNTimes(List.of(17L), 5));
+        assertEquals(15, Day11.numStonesAfterBlinkingNTimes(List.of(17L), 6));
         assertEquals(22, Day11.numStonesAfterBlinkingNTimes(SMALL_INPUT_FILENAME, 6));
         assertEquals(55312, Day11.numStonesAfterBlinkingNTimes(SMALL_INPUT_FILENAME, 25));
         assertEquals(187738, Day11.numStonesAfterBlinkingNTimes(INPUT_FILENAME, 25));
