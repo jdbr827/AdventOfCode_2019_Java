10f60019aef538e261839f7e5350527f6437aed8
jake <reinhartjacob827@gmail.com>
2024-12-22 18:08:46 -0500

    2024 Day 12 Part 1 setup

---
+++ src/year_2024/day_12/Day12.java
@@ -0,0 +1,10 @@
+package year_2024.day_12;
+
+public class Day12 {
+    public Day12(String inputFilename) {
+    }
+
+    public int totalFencingCost() {
+        return 0;
+    }
+}
---
+++ src/year_2024/day_12/Day12Test.java
@@ -0,0 +1,22 @@
+package year_2024.day_12;
+
+import org.junit.jupiter.api.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class Day12Test {
+
+    public static final String INPUT_FILENAME = "src/year_2024/day_12/input_aoc_2024_12.txt";
+    public static final String MINI_INPUT_FILENAME = "src/year_2024/day_12/input_aoc_2024_12_mini.txt";
+    public static final String SMALL_INPUT_FILENAME = "src/year_2024/day_12/input_aoc_2024_12_small.txt";
+    public static final String MEDIUM_INPUT_FILENAME = "src/year_2024/day_12/input_aoc_2024_12_medium.txt";
+
+    @Test
+    public void test_part1() {
+        assertEquals(140, new Day12(MINI_INPUT_FILENAME).totalFencingCost());
+        assertEquals(772, new Day12(SMALL_INPUT_FILENAME).totalFencingCost());
+        assertEquals(1930, new Day12(MEDIUM_INPUT_FILENAME).totalFencingCost());
+        System.out.println(new Day12(INPUT_FILENAME).totalFencingCost());
+    }
+
+}
