c35743a1eef58e1b5c849e1698dc23209cb8076d
jake <reinhartjacob827@gmail.com>
2024-12-27 16:17:14 -0500

    2024 Day 16 Part 2 refactored out racetrack

--- src/year_2024/day_16/Day16.java
+++ src/year_2024/day_16/Day16.java
@@ -1,6 +1,7 @@
 package year_2024.day_16;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 import org.testng.internal.collections.Pair;
 import utils.AOCScanner;
@@ -61,13 +62,17 @@ class State implements Comparable<State> {
     }
 }
 
-public class Day16 {
-    Character[][] grid;
-    CartesianPoint start;
-    CartesianPoint end;
-    int N;
 
-    public Day16(String inputFilename) {
+class Racetrack {
+    Character[][] grid;
+    int N;
+    @Getter
+    CartesianPoint start;
+
+    @Getter
+    CartesianPoint end;
+
+    public Racetrack(String inputFilename) {
         grid = new AOCScanner(inputFilename).scanAsChar2DArray();
         N = grid.length;
         int M = grid[0].length;
@@ -84,6 +89,17 @@ public class Day16 {
         }
     }
 
+    boolean isSpace(CartesianPoint proposed) {
+        return grid[N - proposed.y - 1][proposed.x] != '#';
+    }
+}
+public class Day16 {
+    Racetrack racetrack;
+
+    public Day16(String inputFilename) {
+        racetrack = new Racetrack(inputFilename);
+    }
+
 
     // (i, j) => (j, N-i-1) ... (
 
@@ -91,14 +107,15 @@ public class Day16 {
         Set<Pair<CartesianPoint, CardinalDirection>> found = new HashSet<>();
 
         PriorityQueue<State> pq = new PriorityQueue<>();
-        pq.add(new State(0, start, CardinalDirection.EAST));
-        while (!pq.isEmpty() && !pq.peek().location.equals(end)) {
+        pq.add(new State(0, racetrack.getStart(), CardinalDirection.EAST));
+        while (!pq.isEmpty() && !pq.peek().location.equals(racetrack.getEnd())) {
             //System.out.println(pq);
             State state = pq.poll();
+            assert state != null;
             if (found.add(Pair.of(state.location, state.facing))) {
                 for (CardinalDirection direction : CardinalDirection.values()) {
                     CartesianPoint proposed = state.location.add(direction.velocity);
-                    if (!isWall(proposed) && !found.contains(Pair.of(proposed, direction))) {
+                    if (racetrack.isSpace(proposed) && !found.contains(Pair.of(proposed, direction))) {
                             pq.add(new State(
                                     state.score + 1 + (1000 * rotationsBetween(state.facing, direction)),
                                     proposed,
@@ -120,33 +137,23 @@ public class Day16 {
         return 1;
     }
 
-    private boolean isWall(CartesianPoint proposed) {
-        return grid[N-proposed.y-1][proposed.x] == '#';
-    }
+
 
     public int numGoodSeats() {
         //System.out.println(isWall(new CartesianPoint(12, 9)));
-        Set<Status> found = new HashSet<>();
         Map<Status, Integer> bestScore = new HashMap<>();
         PriorityQueue<State> pq = new PriorityQueue<>();
-        pq.add(new State(0, start, CardinalDirection.EAST));
+        pq.add(new State(0, racetrack.getStart(), CardinalDirection.EAST));
 
         Queue<Status> goodStatuses = new LinkedList<>();
         //int targetScore;
-        while (!pq.isEmpty() && !pq.peek().location.equals(end)) {
+        while (!pq.isEmpty() && !pq.peek().location.equals(racetrack.getEnd())) {
             //System.out.println(pq);
             @NotNull State state = Objects.requireNonNull(pq.poll());
-//            if (state.location.equals(end)) {
-//                if (!bestScore.containsKey(state.getStatus())) {
-//                    targetScore = state.score;
-//                    goodStatuses.add(state.getStatus());
-//                }
-//
-//            }
             if (!bestScore.containsKey(state.getStatus())) {
                 bestScore.put(state.getStatus(), state.score);
                 CartesianPoint proposedForward = state.location.add(state.facing.velocity);
-                if (!isWall(proposedForward) && !bestScore.containsKey(new Status(proposedForward, state.facing))) {
+                if (racetrack.isSpace(proposedForward) && !bestScore.containsKey(new Status(proposedForward, state.facing))) {
                     pq.add(new State(
                           state.score + 1,
                           proposedForward,
@@ -168,38 +175,26 @@ public class Day16 {
 
         assert pq.peek() != null;
         int targetScore = pq.peek().score;
-        //System.out.println(targetScore);
-
         while (!pq.isEmpty() && pq.peek().score == targetScore) {
             State state = pq.poll();
-            //System.out.println(state);
-            if (state.location.equals(end)) {
-                //System.out.println("HI");
+            if (state.location.equals(racetrack.getEnd())) {
                 bestScore.put(state.getStatus(), targetScore);
-                //System.out.println(bestScore.get(state.getStatus()));
                 goodStatuses.add(state.getStatus());
             }
         }
-        //System.out.println(goodStatuses.peek());
-        //System.out.println(bestScore.entrySet());
         Set<CartesianPoint> goodLocations = new HashSet<>();
-        //System.out.println(bestScore.get(new Status(new CartesianPoint(13, 1), CardinalDirection.EAST)));
         while (!goodStatuses.isEmpty()) {
             Status status = goodStatuses.poll();
             goodLocations.add(status.location);
-            //System.out.println(bestScore);
             int score = bestScore.get(status);
             for (CardinalDirection dir : CardinalDirection.values()) {
                 CartesianPoint nbr = status.location.add(dir.opposite().velocity);
-                //System.out.println(nbr + " " + bestScore.get(nbr));
                 Status nxtStatus = new Status(nbr, dir);
-                if (!isWall(nbr) && bestScore.containsKey(nxtStatus) && bestScore.get(nxtStatus) + 1 + (1000*rotationsBetween(dir, status.facing)) == score) {
-                    //System.out.println(nbr + " " + dir + " " + bestScore.get(nxtStatus));
+                if (racetrack.isSpace(nbr) && bestScore.containsKey(nxtStatus) && bestScore.get(nxtStatus) + 1 + (1000*rotationsBetween(dir, status.facing)) == score) {
                     goodStatuses.add(nxtStatus);
                 }
             }
         }
-        //System.out.println(goodLocations);
         return goodLocations.size();
 
     }
